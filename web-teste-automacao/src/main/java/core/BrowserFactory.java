29d55e977dd5195efcd84d8b657ead1be2ddaca5
Matheus Galisteo <matheus.galisteo@uhuu.com>
2019-07-26 17:34:27 -0300

    Inserção das classes de teste e simulações

--- web-teste-automacao/src/main/java/core/BrowserFactory.java
+++ web-teste-automacao/src/main/java/core/BrowserFactory.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -18,15 +19,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserFactory {
 	protected static WebDriver driver;
-	private static final String url = "https://www.sicredi.com.br/html/ferramenta/simulador-investimento-poupanca/";
-	
+	protected static final String url = "https://www.sicredi.com.br/html/ferramenta/simulador-investimento-poupanca/";
+
+	// @Parameters("browser")
 	@BeforeMethod
-	public void Browser(String browser) {
-			browser.equalsIgnoreCase("chrome");
-		{
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-		}
+	public void Browser() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
